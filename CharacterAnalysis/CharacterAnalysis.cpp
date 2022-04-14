c2e678e6526ee69b515896c5183a7260d68cca44
Ryan Bieker <rbieke1@hotmail.com>
2022-04-13 20:03:47 -0600

    Add project files.

---
+++ CharacterAnalysis/CharacterAnalysis.cpp
@@ -0,0 +1,15 @@
+// Ryan Bieker, CS 3060 001, Chapter 10 Programming Challenges, due 4/19/2022, 
+//read stext file and counts uppercase, lowercase, and digits in the file
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream text("text.txt");
+
+
+    text.close();
+}
