cc8d305a2b31a08c2dd067665b322746a0f4728b
Ryan Bieker <rbieke1@hotmail.com>
2022-04-16 20:50:15 -0600

    final changes

--- CharacterAnalysis/CharacterAnalysis.cpp
+++ CharacterAnalysis/CharacterAnalysis.cpp
@@ -6,10 +6,45 @@
 
 using namespace std;
 
+void analysis(ifstream& text);
+
 int main()
 {
     ifstream text("text.txt");
 
+    analysis(text);
 
     text.close();
+}
+
+void analysis(ifstream& text)
+{
+    char character;
+    int upper = 0;
+    int lower = 0;
+    int digits = 0;
+
+    //check for uppercase, lowercase, and digits in text file
+    while (text)
+    {
+        text.get(character);
+
+        if (isupper(character))
+        {
+            upper++;
+        }
+        else if (islower(character))
+        {
+            lower++;
+        }
+        else if (isdigit(character))
+        {
+            digits++;
+        }
+    }
+
+    //print out numbers
+    cout << "Uppercase characters: " << upper << endl;
+    cout << "Lowercase characters: " << lower << endl;
+    cout << "Digits: " << digits << endl;
 }
